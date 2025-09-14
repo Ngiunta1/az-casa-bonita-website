a2eb7dc0d3f756209436f19b00723e57a0893d85
BMoler <BMoler@avxaircraft.com>
2025-09-14 15:29:05 -0500

    more components

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainWrapper from "./components/MainWrapper.tsx";
+import Header from "./components/Header.tsx";
+import About from "./pages/About.tsx";
+import Contact from "./pages/Contact.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const subPages = ["Home", "About", "Contact"];
+const logoSrc = "src/assets/images/az-casa-bonita-logo.png";
+const title = "AZ Casa Bonita";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App/>}/>
-      </Routes>
+      <MainWrapper backgroundImageSrc="src/assets/images/clean-living-room.png">
+        <Header subPages={subPages} logoSrc={logoSrc} title={title} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </MainWrapper>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
