f5c651ddbe1a223231c37568ccbc9642ce00a4f4
BMoler <BMoler@avxaircraft.com>
2025-10-22 23:11:31 -0500

    contact layout updates

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,15 +1,15 @@
 import { Animate } from "../components/Animate";
 import ContactButton from "../components/ContactButton";
 import Tagline from "../components/Tagline";
-import { Phone, Mail } from "lucide-react";
+import { Phone, Mail, ChevronDown } from "lucide-react";
 
 const Contact = () => {
   const phoneNumber = "+1 (602) 884-9751";
   const email = "AZCasaBonitaServices@gmail.com";
   return (
-    <section className="flex h-full w-full items-center justify-center px-30">
-      <div className="flex flex-col items-center">
-        <div className="flex flex-row items-center justify-center gap-12 p-2">
+    <section className="flex h-full w-full items-center justify-center lg:p-20">
+      <div className="flex flex-col h-full items-center justify-center">
+        <div className="flex flex-1 flex-row items-center justify-center gap-12 p-2">
           <Animate mount={{ variant: "fadeUp" }} hover={{ variant: "lift" }}>
             <Tagline
               tagline="Your Dream Clean Just a Call Away"
@@ -32,11 +32,14 @@ const Contact = () => {
             />
           </div>
         </div>
-        <div className="flex justify-end">
-          <h1 className="text-white font-semibold text-3xl tracking-tight top-100">
-            Scroll Down For Scheduling
-          </h1>
-        </div>
+        <Animate mount={{ variant: "fadeBounce", intensity: "extreme" }}>
+          <div className="flex flex-col gap-3 items-center">
+            <h1 className="text-white font-semibold text-3xl tracking-tight top-100">
+              Scroll Down For Scheduling
+            </h1>
+            <ChevronDown color="white" size={36} />
+          </div>
+        </Animate>
       </div>
     </section>
   );
