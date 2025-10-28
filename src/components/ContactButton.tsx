61c252b493d34996a520aab154744bfbc8d036cb
ngiunta <1ngiunta2@gmail.com>
2025-10-27 18:41:35 -0700

    Animated Contact Button Component

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,5 +1,6 @@
 import { div } from "motion/react-client";
 import { useState } from "react";
+import { Animate } from "./Animate";
 
 interface ContactButtonProps {
   icon?: React.ComponentType<any>;
@@ -15,18 +16,20 @@ const ContactButton = ({
   onClick,
 }: ContactButtonProps) => {
   return (
-    <button
-      className="flex bg-[rgba(23,23,23,0.4)] hover:bg-[rgba(23,23,23,0.6)] rounded-xl cursor-pointer items-center"
-      onClick={onClick}
-    >
-      <div className="flex flex-row w-2xl px-4 py-5 gap-4">
-        {Icon && <Icon className="text-white w-auto h-32" strokeWidth={1} />}
-        <div className="flex flex-col justify-center font-noto items-start">
-          <h1 className="text-white font-medium">{mainText}</h1>
-          <h1 className="text-white font-extralight">{subText}</h1>
+    <Animate mount={{ variant: "fadeUp" }} hover={{ variant: "lift" }}>
+      <button
+        className="flex bg-[rgba(23,23,23,0.4)] hover:bg-[rgba(23,23,23,0.6)] rounded-xl cursor-pointer items-center"
+        onClick={onClick}
+      >
+        <div className="flex flex-row w-2xl px-4 py-5 gap-4">
+          {Icon && <Icon className="text-white w-auto h-32" strokeWidth={1} />}
+          <div className="flex flex-col justify-center font-noto items-start">
+            <h1 className="text-white font-medium">{mainText}</h1>
+            <h1 className="text-white font-extralight">{subText}</h1>
+          </div>
         </div>
-      </div>
-    </button>
+      </button>
+    </Animate>
   );
 };
 
