042c7577dd15dea7dcea6822c0a1dea0fb6582dd
BMoler <BMoler@avxaircraft.com>
2026-03-18 22:20:23 -0500

    fixed for mobile 7

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
-import { useRef } from "react";
 
 const subPages = ["Home", "About", "Contact"];
 const logoSrc = "/images/az-casa-bonita-logo.png";
 const title = "AZ Casa Bonita";
 
-// const backgrounds: Record<string, string> = {
-//   "/": "/images/clean-living-room.png",
-//   "/about": "/images/sunset-living-room.png",
-//   "/contact": "/images/kitchen.png",
-// };
-
 const Layout = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-
   return (
-    <div ref={containerRef} className="relative min-h-screen flex flex-col">
+    <div className="relative h-dvh overflow-hidden flex flex-col">
       <Header subPages={subPages} logoSrc={logoSrc} title={title} />
       <Outlet />
     </div>
